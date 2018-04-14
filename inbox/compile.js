f7fa03c2c49d6de467d8061ef4f7fda5359f3956
hifarrer <hifarrer@gmail.com>
2018-04-14 17:17:16 -0500

    final

--- inbox/compile.js
+++ inbox/compile.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const fs = require('fs');
-const solc = require('solc');
+const solc = require('solc'); //solidity compiler
 
 
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol'); //contracts folder and inbox file
@@ -8,3 +8,4 @@ const source = fs.readFileSync(inboxPath,'utf8');
 
 //console.log(solc.compile(source,1));//1 is the number of different contracts we are trying to compile
 module.exports = solc.compile(source,1).contracts[':Inbox'];
+//:Inbox , the semicolon is incase we need to specify a file like myinbox:Inbox
